279ef1fc047fcef543c92e8645d114b9fbee2a7b
aldowjy <aldowjy12@gmail.com>
2020-03-21 12:51:20 +0700

    neh materi

--- resources/views/admin/materi.blade.php
+++ resources/views/admin/materi.blade.php
@@ -10,7 +10,7 @@
                     <div class="col-12">
                         <form action="" method="post">
                             <div class="form-group row">
-                                <label for="judul" class="col-3 inputRequired">Judul*</label>
+                                <label for="judul" class="col-2 inputRequired">Judul*</label>
                                 <div class="col-1">:</div>
                                 <input type="text" class="form-control col-7" id="judul" placeholder="Enter Judul" name="judul" required>
                             </div>
@@ -43,9 +43,7 @@
                                     <td>Judul_Name</td>
                                     <td>Tanggal_date</td>
                                     <td style="display: flex; justify-content: space-around;">
-                                        <form id="button-yes-judul{{$judul->judul_id}}" class="submitForm" action="/admin/approve/judul" method="POST">@csrf<input for="judul" name="judul" value="{{$judul->judul_id}}" style="display:none"><input for="adm" name="adm" value="{{$admin->adm_id}}" style="display:none">
-                                            <button type="submit" class="btn btn-outline-success btn-sm btn-pill btnSubmit py-2 px-3">Edit</button>
-                                        </form>
+                                        <button type="submit" class="btn btn-outline-success btn-sm btn-pill btnSubmit py-2 px-3" data-toggle="modal" data-target="#editMateri">Edit</button>
                                         <form id="button-no-judul{{$judul->judul_id}}" class="submitForm" action="/admin/disapprove/judul" method="POST">@csrf<input for="judul" name="judul" value="{{$judul->judul_id}}" style="display:none"><input for="adm" name="adm" value="{{$admin->adm_id}}" style="display:none">
                                             <button type="submit" class="btn btn-outline-danger btn-sm btn-pill btnSubmit py-2 px-3">Delete</button>
                                         </form>
@@ -54,6 +52,42 @@
                                 @endif
                             </tbody>
                         </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="modal fade" id="editMateri" tabindex="-1" role="dialog" aria-labelledby="modalTitle"
+        aria-hidden="true">
+        <div class="modal-dialog modal-xl modal-dialog-centered" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="modalTitle">Judul_Materi</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <div>
+                        <form action="" method="POST" class="submitForm" enctype="multipart/form-data">
+                            <div class="preview">
+                                <p>No files currently selected for upload</p>
+                            </div>
+                            <div class="form-group row">
+                                <label for="file_upload" class="col-4">Choose file to upload (TXT, PNG, 3GP)</label>
+                                <label class="col-1 col-form-label">:</label>
+                                <input class="col-6" type="file" id="file_upload" name="file_upload" accept=".txt, .png, .3gp">
+                            </div>
+                            <div class="form-group row">
+                                <label for="paragraph" class="col-4">Paragraph</label>
+                                <label class="col-1 col-form-label">:</label>
+                                <textarea class="form-control col-5" id="paragraph" rows="5"></textarea>
+                            </div>
+                            <div class="modal-footer">
+                                <button id="btnSubmit" type="button" class="btn btn-success">Send</button>
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
