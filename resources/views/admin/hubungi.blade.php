a20b32f6b948d063c6503c3af0ce62bbd9a555a5
minatorisato <36133909+minatorisato@users.noreply.github.com>
2020-05-25 15:05:28 +0700

    contact us

--- resources/views/admin/hubungi.blade.php
+++ resources/views/admin/hubungi.blade.php
@@ -9,9 +9,9 @@
                     <div class="col-4">
                         <a href="">
                             <div class="mx-3">
-                                <img src="https://pbs.twimg.com/profile_images/413090483162734592/BTChv6kh_400x400.jpeg" width="150" height="150" class="d-inline-block align-top my-4" alt="">
-                                <h4>Bambang</h4>
-                                <h6>bambang@email.com</h6>
+                                <img src="/assets/img/sitiaisyah.jpeg" width="150" height="150" class="d-inline-block align-top my-4" alt="">
+                                <h4>Siti Aisyah</h4>
+                                <h6>sitiaisyah@email.com</h6>
                                 <h6>081234567890</h6>
                             </div>
                         </a>
@@ -19,9 +19,9 @@
                     <div class="col-4">
                         <a href="">
                             <div class="mx-3">
-                                <img src="https://pbs.twimg.com/profile_images/413090483162734592/BTChv6kh_400x400.jpeg" width="150" height="150" class="d-inline-block align-top my-4" alt="">
-                                <h4>Kusnadi</h4>
-                                <h6>kusnadi@email.com</h6>
+                                <img src="/assets/img/nurulqoyimah.jpeg" width="150" height="150" class="d-inline-block align-top my-4" alt="">
+                                <h4>Nurul Qoyimah</h4>
+                                <h6>nurulqoyimah@email.com</h6>
                                 <h6>081234567890</h6>
                             </div>
                         </a>
@@ -29,9 +29,9 @@
                     <div class="col-4">
                         <a href="">
                             <div class="mx-3">
-                                <img src="https://pbs.twimg.com/profile_images/413090483162734592/BTChv6kh_400x400.jpeg" width="150" height="150" class="d-inline-block align-top my-4" alt="">
-                                <h4>Sutisna</h4>
-                                <h6>sutisna@email.com</h6>
+                                <img src="/assets/img/fatimahhumairah.jpeg" width="150" height="150" class="d-inline-block align-top my-4" alt="">
+                                <h4>Fatimah Humairah</h4>
+                                <h6>fatimahhumairah@email.com</h6>
                                 <h6>081234567890</h6>
                             </div>
                         </a>
