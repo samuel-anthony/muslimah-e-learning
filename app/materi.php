f7da84396175de3ab5a8b6e7f23e610f61c92ef1
Lunalisha <61875089+Lunalisha@users.noreply.github.com>
2020-06-03 10:26:33 -0500

    tambah comment

--- app/materi.php
+++ app/materi.php
@@ -7,6 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class materi extends Model
 {
     public function materi_details(){
-        return $this->hasMany('App\materi_detail','materi_id','id');
+        return $this->hasMany('App\materi_detail','materi_id','id'); //(nama kolom dituju, id yg dituju, id model ini)
     }
 }
--- app/ujian.php
+++ app/ujian.php
@@ -4,12 +4,13 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+//week sama duration
 class ujian extends Model
 {
     public function user_ujian(){
         return $this->belongsTo('App\user_ujian','id','ujian_id');
     }
     public function pertanyaans(){
-        return $this->hasMany('App\pertanyaan','ujian_id','id');
+        return $this->hasMany('App\pertanyaan','ujian_id','id'); //admin
     }
 }
